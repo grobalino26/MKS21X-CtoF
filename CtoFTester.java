1a08ed466c21fa32588fa829c4084cd56abb36b1
Gavin Robalino <78228662+grobalino26@users.noreply.github.com>
2021-09-23 18:44:00 -0400

    Create CtoFTester.java

---
+++ CtoFTester.java
@@ -0,0 +1,13 @@
+//The functions should have doubles as parameters. celsiusToFarenheit should take celsius and return farenheit. farenheitToCelsius should take farenheit and return celsius.
+public class CtoFTester.java {
+  public static void main (String[] args){
+    System.out.println(celsiusToFarenheit(30.0);
+    System.out.println(celsiusToFarenheit(30.0);
+  }
+  public static double celsiusToFarenheit (double celsius) {
+    return celsius * (9/5) + 32
+  }
+  public static double farenheitToCelsius (double farenheit) {
+    return (farenheit - 32) * (5/9)
+  }
+}
